0a2384e5f0a87462366c768a17f7a58d850a0140
Iris Gwen J. Caguiat <redseijuro@gmail.com>
2021-02-05 14:36:06 +0800

    deploy

---
+++ precache-manifest.144c50a1a6edd4268843da07971aef19.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c955314e9b276936495",
+    "url": "/css/app.406ff47c.css"
+  },
+  {
+    "revision": "fc505a16c73f9e3f9e0d",
+    "url": "/css/chunk-34b3576d.39de0a21.css"
+  },
+  {
+    "revision": "2a13ae207049a56901ec",
+    "url": "/css/chunk-71ef0f55.d991fcfc.css"
+  },
+  {
+    "revision": "d705e327b339d3accab5",
+    "url": "/css/chunk-a6b2f97c.8ebdba4a.css"
+  },
+  {
+    "revision": "e96a027be926d974654f",
+    "url": "/css/chunk-vendors.487574a2.css"
+  },
+  {
+    "revision": "489aa932ac4512bd1181bf5aa22e9b34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c955314e9b276936495",
+    "url": "/js/app.33834ba8.js"
+  },
+  {
+    "revision": "fc505a16c73f9e3f9e0d",
+    "url": "/js/chunk-34b3576d.1fddfa2b.js"
+  },
+  {
+    "revision": "2a13ae207049a56901ec",
+    "url": "/js/chunk-71ef0f55.c637d088.js"
+  },
+  {
+    "revision": "1a6404410fd2a87d9b2e",
+    "url": "/js/chunk-82205456.c677739b.js"
+  },
+  {
+    "revision": "d705e327b339d3accab5",
+    "url": "/js/chunk-a6b2f97c.e4c52bd0.js"
+  },
+  {
+    "revision": "e96a027be926d974654f",
+    "url": "/js/chunk-vendors.2e168f95.js"
+  },
+  {
+    "revision": "419ee7d8f98576c41cb9d527f8e04314",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
